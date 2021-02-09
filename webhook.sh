b125ca8428d14e76b4a747c322d2527677b2e299
Oleksiy Romanenko <oleksiy.romanenko@gmail.com>
2021-02-09 10:04:21 +0200

    add missing \ in webhook.sh

--- webhook.sh
+++ webhook.sh
@@ -4,7 +4,7 @@ if [[ -z "$BOT_TOKEN" ]]; then
   echo "BOT_TOKEN variable is not set"
 else
   curl -X POST \
-    -H "Content-Type: application/json"
+    -H "Content-Type: application/json" \
     -d '{"url": "https://fortnit-elves-bot.ew.r.appspot.com"}' \
     "https://api.telegram.org/bot$BOT_TOKEN/setWebhook"
 fi
